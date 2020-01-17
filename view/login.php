b6ac86daa853652138f32e830e530e7d19670d4f
Nguyễn Mạnh <54877245+manh74@users.noreply.github.com>
2020-01-17 08:52:56 +0700

    Add files via upload

---
+++ view/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="post" action="../controller/usercontroller.php">
+		<input type="text" name="username">
+		<input type="text" name="password">
+		<button type="submit" name="login">Login</button>
+	</form>
+</body>
+</html>
